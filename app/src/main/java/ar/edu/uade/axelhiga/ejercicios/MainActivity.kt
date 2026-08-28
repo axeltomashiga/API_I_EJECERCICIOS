58548f8e5a3d7e1fd8b101d5744b67b2c19663b6
axel <axeltomashiga@gmail.com>
2026-08-28 20:56:29 -0300

    Ejercicio 2 completado

--- app/src/main/java/ar/edu/uade/axelhiga/ejercicios/MainActivity.kt
+++ app/src/main/java/ar/edu/uade/axelhiga/ejercicios/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ar.edu.uade.axelhiga.ejercicios.ui.theme.EjerciciosTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +23,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EjerciciosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Presentacion ()
                 }
             }
         }
@@ -43,5 +43,23 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     EjerciciosTheme {
         Greeting("Android")
+    }
+}
+
+
+@Composable
+fun Presentacion() {
+    Column(modifier = Modifier.padding(start = 20.dp, top = 50.dp, end = 20.dp)) {
+        Text("Nombre")
+        Text("Carrera", fontWeight = FontWeight.Bold)
+        Text("Tecnología que el estudiante quiera aprender")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PresentacionPreview () {
+    EjerciciosTheme() {
+        Presentacion()
     }
 }
